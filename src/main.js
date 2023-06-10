8776e2377269f4b6cbae43fae0451061e21321ea
Umesh <Umeshtambat55@gmail.com>
2023-06-10 10:16:37 +0530

    record added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-async function main() {
+async function insertRecord() {
   // due to await present insite we added async keyword
 
   const uri = "mongodb://0.0.0.0:27017"; //connect with server
@@ -9,12 +9,17 @@ async function main() {
   const db = client.db("mydb"); //connect with client
   const message = db.collection("message"); //connect with collection message
 
-  await message.insertOne({}); //add data in collection message
+  let jsonDocument = {message: "Hello World", to: 'rohit', from:'tejas'};
+  await message.insertOne({jsonDocument}); //add data in collection message
   // insert is async method also return type is promise so added await
 
   //close the connection otherwise it will never stop
   await client.close(); // return type is promise to add await
   console.log("Record Added");
+}
+
+async function main() {
+await insertRecord();
 }
 
 main();
